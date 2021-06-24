47e5fcad59762980bf8a6e09e31543666bddd9f4
khoirulbads <irulad151@gmail.com>
2021-06-24 17:59:45 +0700

    tentang fix

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -9,9 +9,9 @@
     <title>Dumas | Pengaduan Masyarakat </title>
 
 
-    <link rel="apple-touch-icon" href="https://latbang-bkkbnjatim.com/wp-content/uploads/2020/06/logo-bkkbn-new-normal.png">
-    <link rel="shortcut icon" type="image/x-icon" href="https://latbang-bkkbnjatim.com/wp-content/uploads/2020/06/logo-bkkbn-new-normal.png">
-    
+   <link rel="apple-touch-icon" href="assets/back/app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/login.png">
+ 
 
     <link rel='stylesheet' href='assets/website/revslider/public/assets/css/settings.css' type='text/css' media='all'>
     <link rel='stylesheet' href='assets/website/css/animation.css' type='text/css' media='all'>
