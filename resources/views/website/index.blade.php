5a02b4e218aa8a0f79cecaede8526f5b25d14e70
fariez11 <69473438+fariez11@users.noreply.github.com>
2021-06-28 12:17:21 +0700

    Update index.blade.php

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -63,7 +63,7 @@ $sosmed = DB::select("select * from sosmed");
             	<li class="menu-item"><a href="/">Beranda</a></li>
                 <li class="menu-item"><a href="/tentang">Apa itu Dumas??</a></li>
                 <li class="menu-item"><a href="/kontak">Hubungi Kami</a></li>
-                <li class="menu-item"><a href="/auth">MASUK</li>
+                <li class="menu-item"><a href="{{ url('/auth') }}">MASUK</li>
                 <li>
                     @foreach ($setting as $data)
                     <a href="https://api.whatsapp.com/send?phone=62{{$data->NO_PONSEL}}">
@@ -87,7 +87,7 @@ $sosmed = DB::select("select * from sosmed");
             <div class="top_bar  hasbg ">
 
                 <div id="mobile_nav_icon">
-                <a href="/auth">
+                <a href="{{ url('/auth') }}">
                         <div class="header_action">
                             MASUK 
                         </div>
@@ -114,7 +114,7 @@ $sosmed = DB::select("select * from sosmed");
                         <img src="assets/website/images/newlogo1.png" alt="" width="50" height="50">
                     </a>
                     <!-- End logo -->
-                    <a href="/auth">
+                    <a href="{{ url('/auth') }}">
                         <div class="header_action">
                             MASUK 
                         </div>
